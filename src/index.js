23ebc5f338fee686d5d90af37707bf9b19ced06b
kboverhuis <kboverhuis@gmail.com>
2024-08-02 16:54:31 -0500

    Added the API intergration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-function generateGratitude(event) {
-  event.preventDefault();
-
+function displayGratitude(response) {
+  console.log("Gratitude generated");
   new Typewriter("#gratitude", {
-    strings:
-      "Having a place to go is a home. Having someone to love is a family. Having both is a blessing.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 50,
     cursor: "",
   });
+}
+
+function generateGratitude(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "8908d7b1834oa44093b7b64af0t393f3";
+  let context =
+    "You have a positive and grateful outlook and believes in the importance of gratitude and minefullness.  Create a quality response of gratitude using the user instructions.  Create a two line response using HTML format with a <br /> after each sentence.  Please add at the end of the response <strong> SheCodes AI ";
+  let prompt = `User Instructions: Generate a positive response of gratitude about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating Gratitude");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayGratitude);
 }
 
 let gratitudeFormElement = document.querySelector("#gratitude-generator-form");
