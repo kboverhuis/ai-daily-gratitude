b005597bda067ad8ea79e588d57f5a5a1373cd5c
kboverhuis <kboverhuis@gmail.com>
2024-08-02 11:39:20 -0500

    Added the basic form handling and added the typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateGratitude(event) {
+  event.preventDefault();
 
+  new Typewriter("#gratitude", {
+    strings:
+      "Having a place to go is a home. Having someone to love is a family. Having both is a blessing.",
+    autoStart: true,
+    delay: 50,
+    cursor: "",
+  });
+}
+
+let gratitudeFormElement = document.querySelector("#gratitude-generator-form");
+gratitudeFormElement.addEventListener("submit", generateGratitude);
